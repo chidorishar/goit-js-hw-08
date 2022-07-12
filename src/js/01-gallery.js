6caec6c49f199fff466dbdf6c0f1455427998043
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-12 20:41:23 +0300

    Refactored gallery creation code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,14 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import galleryItemTmpl from './gallery-item.hbs';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const galleryRootEl = document.querySelector('.gallery');
 
-const galleryMarkup = galleryItems.map(item =>
-  createGalleryItem({
-    previewImage: item.preview,
-    fullsizeImage: item.original,
-    description: item.description,
-  })
-);
-
-galleryRootEl.innerHTML = galleryMarkup.join(' ');
+galleryRootEl.innerHTML = galleryItemTmpl({ galleryItems });
 var lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
-
-function createGalleryItem({ previewImage, fullsizeImage, description }) {
-  return `<a class="gallery__item" href=${fullsizeImage}>
-            <img class="gallery__image" src=${previewImage} alt=${description} />
-        </a>`;
-}

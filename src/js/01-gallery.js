6e9678c2576e8a2d1bb603fc94b4a09182a48eb5
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-12 19:42:15 +0300

    Added gallery js code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRootEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(item =>
+  createGalleryItem({
+    previewImage: item.preview,
+    fullsizeImage: item.original,
+    description: item.description,
+  })
+);
+
+galleryRootEl.innerHTML = galleryMarkup.join(' ');
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function createGalleryItem({ previewImage, fullsizeImage, description }) {
+  return `<a class="gallery__item" href=${fullsizeImage}>
+            <img class="gallery__image" src=${previewImage} alt=${description} />
+        </a>`;
+}

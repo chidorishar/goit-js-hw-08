086ab5bf420a0f5ba3507b3724ce6878e4ea1970
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-13 11:21:29 +0300

    Added feedback form basic code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const throttle = require('lodash.throttle');
 
+const FORM_DATA_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+let formData = {};
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+(() => {
+  formData = JSON.parse(localStorage.getItem(FORM_DATA_KEY));
+  if (formData) {
+    Object.keys(formData).forEach(key =>
+      formData[key] ? (formEl.elements[key].value = formData[key]) : null
+    );
+  } else {
+    formData = {};
+  }
+})();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  [...formEl.elements].forEach(el => (el.value = ''));
+  localStorage.removeItem(FORM_DATA_KEY);
+  console.log(formData);
+}
